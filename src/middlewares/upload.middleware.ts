766359ec358e110b6eb61edfe1072858615563b9
IzayoiS <iqbalmhasby54@gmail.com>
2025-04-15 08:01:34 +0700

    fix: upload file

--- src/middlewares/upload.middleware.ts
+++ src/middlewares/upload.middleware.ts
@@ -1,28 +1,15 @@
 import multer from 'multer';
-import path from 'path';
 
 const whitelist = ['image/png', 'image/jpeg', 'image/jpg'];
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './tmp/images');
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-    cb(
-      null,
-      file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname),
-    );
-  },
-});
+const storage = multer.memoryStorage();
 
 export const uploadImage = multer({
-  storage: storage,
+  storage,
   fileFilter: (req, file, cb) => {
     if (!whitelist.includes(file.mimetype)) {
-      return cb(new Error('file is not allowed'));
+      return cb(new Error('File type not allowed'));
     }
-
     cb(null, true);
   },
 });
